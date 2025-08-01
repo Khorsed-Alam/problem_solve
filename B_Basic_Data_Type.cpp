13b7e6c90249a67b3dc0f85d5d13e20106993fb9
Khorsed Alam <khorsedalam0472@gmail.com>
2025-08-01 17:23:34 +0600

    Sheet1. B. Basic Data Type

---
+++ B_Basic_Data_Type.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    long long nn;
+    char ch;
+    double d;
+    float f;
+    cin>> n>>nn>>ch>>f>>d;
+    cout<<n<<endl<<nn<<endl<<ch<<endl<<f<<endl<<d<<endl;
+    return 0;
+}

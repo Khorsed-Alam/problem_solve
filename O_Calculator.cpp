3600195c1541f4b7e1f5a7f900e7ed8ac7b1a794
Khorsed Alam <khorsedalam0472@gmail.com>
2025-08-04 00:58:29 +0600

    Sheet1_ O.Calculator

---
+++ O_Calculator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int A,B;
+   char S;
+
+    cin>>A>>S>>B;
+
+    if(S=='+'){
+        cout<< A+B;
+    }
+    else if(S=='-'){
+        cout<< A-B;
+    }else if(S== '*' ){
+        cout<< A*B;
+    }else{
+        cout<< A/B;
+    }
+    return 0;
+}

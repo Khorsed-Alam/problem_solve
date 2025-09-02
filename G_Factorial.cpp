f21a9867aecf8efd4db1459fa2b48c09923b81d4
Khorsed Alam <khorsedalam0472@gmail.com>
2025-09-03 00:21:46 +0600

    Sheet 2 G_Factorial

---
+++ G_Factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;   
+    while (T--) {
+        int N;
+        cin >> N;
+
+        long long fact = 1;
+        for (int i = 2; i <= N; i++) {
+            fact *= i;
+        }
+
+        cout << fact << endl;
+    }
+    return 0;
+}
